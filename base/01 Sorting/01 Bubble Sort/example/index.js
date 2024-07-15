17be2c4afcb50727c75c81b5c569d8d6c4844a5e
190101101 <190101101@mail.ru>
2024-07-16 02:17:46 +0400

    base\01 Sorting\02 Selection Sort

--- base/01 Sorting/01 Bubble Sort/example/index.js
+++ base/01 Sorting/01 Bubble Sort/example/index.js
@@ -1,8 +1,12 @@
 const bubbleSort = (p1) => {
+  // Внешний цикл проходит по всему массиву
   for (let i = 0; i < p1.length; i++) {
+    // Внутренний цикл для сравнения соседних элементов
     for (let j = 0; j < p1.length - 1 - i; j++) {
-      // console.log(p1);
+      // Сравниваем текущий элемент с следующим элементом
+      console.log(p1);
       if (p1[j] > p1[j + 1]) {
+        // Если текущий элемент больше, меняем их местами
         const temp = p1[j];
         p1[j] = p1[j + 1];
         p1[j + 1] = temp;
@@ -11,7 +15,6 @@ const bubbleSort = (p1) => {
   }
 };
 
-const p1 = [ 2, 5, 4, 3 ];
+const p1 = [2, 5, 4, 3];
 bubbleSort(p1);
 console.log(p1);
-
