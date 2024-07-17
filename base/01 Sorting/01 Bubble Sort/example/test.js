cf6fe99236ccc4d04913a3b94089b2dd8133615f
190101101 <190101101@mail.ru>
2024-07-17 04:34:33 +0400

    update

--- base/01 Sorting/01 Bubble Sort/example/test.js
+++ base/01 Sorting/01 Bubble Sort/example/test.js
@@ -1,4 +1,15 @@
-const bubbleSort = (arr) => {};
+const bubbleSort = (arr) => {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+};
 
 let arr = [5, 4, 3, 2, 1];
 console.log(`[+] => [${arr}]`);
