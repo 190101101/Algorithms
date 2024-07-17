860b6119506787b80c482c0dcfceb09090868d4c
190101101 <190101101@mail.ru>
2024-07-17 21:35:12 +0400

    base\01 Sorting\03 Insertion Sort

--- base/01 Sorting/01 Bubble Sort/example/test.js
+++ base/01 Sorting/01 Bubble Sort/example/test.js
@@ -1,17 +1,6 @@
-const bubbleSort = (arr) => {
-  let n = arr.length;
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = temp;
-      }
-    }
-  }
-};
+const bubbleSort = (arr) => {};
 
 let arr = [5, 4, 3, 2, 1];
-console.log(`[+] => [${arr}]`);
+console.log(arr);
 bubbleSort(arr);
-console.log(`[+] => [${arr}]`);
+console.log(arr);
