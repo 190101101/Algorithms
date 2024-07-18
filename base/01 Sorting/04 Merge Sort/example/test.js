b7f9b38fc32578f98ad22c3d02b9c59aed17258c
190101101 <190101101@mail.ru>
2024-07-18 23:46:56 +0400

    18.07.2024 merge sort

--- base/01 Sorting/04 Merge Sort/example/test.js
+++ base/01 Sorting/04 Merge Sort/example/test.js
@@ -1,34 +1,6 @@
-const merge = (left, right) => {
-  let resultArray = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
+const merge = (left, right) => {};
 
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      resultArray.push(left[leftIndex]);
-      leftIndex++;
-    } else {
-      resultArray.push(right[rightIndex]);
-      rightIndex++;
-    }
-  }
-
-  return resultArray
-    .concat(left.slice(leftIndex))
-    .concat(right.slice(rightIndex));
-};
-
-const mergeSort = (array) => {
-  if (array.length <= 1) {
-    return array;
-  }
-
-  let middle = Math.floor(array.length / 2);
-  let left = array.slice(0, middle);
-  let right = array.slice(middle);
-
-  return merge(mergeSort(left), mergeSort(right));
-};
+const mergeSort = (array) => {};
 
 const arr = [38, 27, 43, 3, 9, 82, 11, 64];
 const sortedArray = mergeSort(arr);
