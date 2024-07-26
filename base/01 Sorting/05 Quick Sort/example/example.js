12d9a85efe3a3da96e637743bdb02403dee04325
190101101 <190101101@mail.ru>
2024-07-26 05:11:18 +0400

    base\03 Dynamic Programming\01 Fibonacci Sequence

--- base/01 Sorting/05 Quick Sort/example/example.js
+++ base/01 Sorting/05 Quick Sort/example/example.js
@@ -1,18 +1,12 @@
 const quickSort = (array) => {
-  if (array.length <= 1) {
-    return array;
-  }
+  if (array.length <= 1) return array
 
   const pivot = array[array.length - 1];
   const left = [];
   const right = [];
 
   for (let i = 0; i < array.length - 1; i++) {
-    if (array[i] < pivot) {
-      left.push(array[i]);
-    } else {
-      right.push(array[i]);
-    }
+    array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
   }
 
   return [...quickSort(left), pivot, ...quickSort(right)];
